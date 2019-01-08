9e0b8e3f872b296a28e5f0064a68b3ff175e7ace
Víctor Galvín Coronil <you@example.com>
2019-01-08 21:30:45 +0100

    Práctica 4

---
+++ LOOPBACK03/AD_Control.vhd
@@ -0,0 +1,90 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.11.2018 10:45:52
+-- Design Name: 
+-- Module Name: AD_Control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity AD_Control is
+    Port ( AD_CS_O : out STD_LOGIC;
+           AD_SCLK_O : out STD_LOGIC;
+           AD_SDATA_I : in STD_LOGIC;
+           CLK_I : in STD_LOGIC;
+           RESET_I : in STD_LOGIC;
+           AD_DATA_O: out STD_LOGIC_VECTOR (15 downto 0));
+end AD_Control;
+
+architecture Behavioral of AD_Control is
+
+signal COUNT: integer range 0 to 19; --Señal interna que representa al contador
+signal REG: STD_LOGIC_VECTOR (15 downto 0);
+
+begin
+
+process (RESET_I,CLK_I) --Contador
+begin
+   if RESET_I ='1' then
+     COUNT <= 0;
+   elsif (CLK_I'event and CLK_I = '1') then --Es lo mismo que decir rising_edge(CLK_I), que significa que compruebe el flanco de subida de la señal CLK_I
+      if COUNT=19 then
+         COUNT <=0;
+      else
+         COUNT <= COUNT + 1;
+      end if;
+   end if;
+end process;
+
+process (RESET_I,CLK_I) -- Señal CS
+begin
+   if RESET_I ='1' then
+     AD_CS_O <='0';
+   elsif (CLK_I'event and CLK_I = '1') then --Es lo mismo que decir rising_edge(CLK_I), que significa que compruebe el flanco de subida de la señal CLK_I
+      if COUNT=0 then
+         AD_CS_O <='1';
+      else
+         AD_CS_O <= '0';
+      end if;
+   end if;
+end process;
+
+process (RESET_I,CLK_I) --Lectura de SDATA
+begin
+   if RESET_I ='1' then         --No hace falta el reset
+     REG  <= "0000000000000000";
+   elsif (CLK_I'event and CLK_I = '0') then  
+    if COUNT > 1 and COUNT < 18 then
+        REG(0) <= AD_SDATA_I;
+        REG (15 downto 1) <= REG (14 downto 0);
+    elsif COUNT = 18 then
+        AD_DATA_O  <= "0" & REG(14 downto 0);
+    end if;
+   end if;
+end process;
+
+end Behavioral;
---
+++ LOOPBACK03/LOOPBACK.vhd
@@ -0,0 +1,118 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.10.2018 11:05:14
+-- Design Name: 
+-- Module Name: LOOPBACK - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LOOPBACK is
+    Port ( CLK_I : in STD_LOGIC;
+           RESET_I : in STD_LOGIC;
+           AD_SDATA_I : in STD_LOGIC;
+           AD_SCLK_O : out STD_LOGIC;
+           AD_CS_O : out STD_LOGIC;
+           DA_SYNC_O : out STD_LOGIC;
+           DA_SCLK_O : out STD_LOGIC;
+           DA_DIN_O : out STD_LOGIC
+     );
+
+end LOOPBACK;
+
+architecture Behavioral of LOOPBACK is
+
+signal SCLK : STD_LOGIC;  --Declaramos señal interna
+signal RESET: STD_LOGIC;  --Declaramos señal interna
+signal LOCKED: STD_LOGIC; --Declaramos señal interna
+signal DATA : STD_LOGIC_VECTOR (15 downto 0);
+
+component SCLK_GEN is  -- Divisor de frecuencia
+  port (
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+ 
+end component;
+
+component DA_Control is             --Convertidor DA
+    Port ( RESET_I : in STD_LOGIC;
+           CLK_I : in STD_LOGIC;
+           SYNC_O : out STD_LOGIC;
+           DIN_O : out STD_LOGIC;
+           DATA_I : in STD_LOGIC_VECTOR (15 downto 0));
+end component;
+
+component AD_Control is             --Convertidor AD
+    Port ( AD_CS_O : out STD_LOGIC;
+           AD_SCLK_O : out STD_LOGIC;
+           AD_SDATA_I : in STD_LOGIC;
+           CLK_I : in STD_LOGIC;
+           RESET_I : in STD_LOGIC;
+           AD_DATA_O : out STD_LOGIC_VECTOR (15 downto 0));
+end component;
+
+begin
+
+
+Instancia_0: SCLK_GEN
+  port map(
+    clk_out1 => SCLK,
+    reset => RESET_I,
+    locked => LOCKED,
+    clk_in1 => CLK_I
+  );
+  
+  
+Instancia_1: DA_Control              
+      port map ( 
+             RESET_I => RESET,
+             CLK_I => SCLK,
+             SYNC_O => DA_SYNC_O,
+             DIN_O => DA_DIN_O,
+             DATA_I => DATA
+  );  
+  
+RESET <= RESET_I or not LOCKED; -- RESET es interno. Si pulsamos RESET_I (externo) o LOCKED está a nivel bajo, mantenemos toda la parte sincrona parada, para evitar que avance mientras el reloj no es estable
+AD_SCLK_O <= SCLK;
+DA_SCLK_O <= SCLK; 
+
+Instancia_2: AD_Control              
+      port map ( 
+             RESET_I => RESET,
+             CLK_I => SCLK,
+             AD_CS_O => AD_CS_O,
+             AD_SCLK_O => AD_SCLK_O,
+             AD_SDATA_I => AD_SDATA_I, 
+             AD_DATA_O => DATA           
+  ); 
+ 
+end Behavioral;
+
+
