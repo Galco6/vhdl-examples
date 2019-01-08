dc91e0f66813f832e437bfd2afe7bfbc6d8dfa68
Víctor Galvín Coronil <you@example.com>
2019-01-08 21:34:54 +0100

    Práctica 6

---
+++ LP_FILTER02/DA_Control.vhd
@@ -0,0 +1,93 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 24.10.2018 11:02:59
+-- Design Name: 
+-- Module Name: DA_Control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DA_Control is
+    Port ( RESET_I : in STD_LOGIC;
+           CLK_I : in STD_LOGIC;
+           SYNC_O : out STD_LOGIC;
+           DIN_O : out STD_LOGIC;
+           DATA_I : in STD_LOGIC_VECTOR (15 downto 0)
+   );
+end DA_Control;
+
+architecture Behavioral of DA_Control is
+
+signal COUNT: integer range 0 to 19; --Señal interna que representa al contador
+signal REG: STD_LOGIC_VECTOR (15 downto 0);
+
+begin
+
+process (RESET_I,CLK_I)
+begin
+   if RESET_I ='1' then
+     COUNT <=0;
+   elsif (CLK_I'event and CLK_I = '1') then --Es lo mismo que decir rising_edge(CLK_I), que significa que compruebe el flanco de subida de la señal CLK_I
+      if COUNT=19 then
+         COUNT <=0;
+      else
+         COUNT <= COUNT + 1;
+      end if;
+   end if;
+end process;
+
+process (RESET_I,CLK_I)
+begin
+   if RESET_I ='1' then
+     REG <= "0000000000000000";
+   elsif (CLK_I'event and CLK_I = '1') then --Es lo mismo que decir rising_edge(CLK_I), que significa que compruebe el flanco de subida de la señal CLK_I
+      if COUNT=1 then
+         REG <= DATA_I;             -- Carga en paralelo de los 16 bits
+      elsif COUNT > 1 then
+         REG (15 downto 1) <= REG (14 downto 0); --Desplazamiento a la izquierad
+      end if;
+   end if;
+end process;
+
+DIN_O <= REG(15); --Mandamos por la salida el bit mas significativo. Esta asignacion podemos hacerla al principio del todo, no importa su posicion
+
+
+process (RESET_I,CLK_I)
+begin
+   if RESET_I ='1' then
+     SYNC_O <='0';
+   elsif (CLK_I'event and CLK_I = '0') then --Es lo mismo que decir rising_edge(CLK_I), que significa que compruebe el flanco de subida de la señal CLK_I
+      if COUNT=0 then
+         SYNC_O <='1';
+      else
+         SYNC_O <= '0';
+      end if;
+   end if;
+end process;
+
+
+end Behavioral;
---
+++ LP_FILTER02/Filter.vhd
@@ -0,0 +1,102 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.11.2018 10:57:08
+-- Design Name: 
+-- Module Name: Filter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Filter is
+    Port ( CLK : in STD_LOGIC;
+           DATA_I : in STD_LOGIC_VECTOR (15 downto 0);
+           RST : in STD_LOGIC;
+           START_I : in STD_LOGIC;
+           DATA_O : out STD_LOGIC_VECTOR (15 downto 0)
+          );
+end Filter;
+
+architecture Behavioral of Filter is
+
+type MATRIZ is array (0 to 18) of UNSIGNED(11 downto 0);
+constant COEFF: MATRIZ := ( to_unsigned(24, 12),
+                            to_unsigned(75, 12),
+                            to_unsigned(107, 12),
+                            to_unsigned(177, 12),
+                            to_unsigned(244, 12),
+                            to_unsigned(320, 12),
+                            to_unsigned(389, 12),
+                            to_unsigned(446, 12),
+                            to_unsigned(483, 12),
+                            to_unsigned(497, 12),
+                            to_unsigned(483, 12),
+                            to_unsigned(446, 12),                          
+                            to_unsigned(389, 12),
+                            to_unsigned(320, 12),
+                            to_unsigned(244, 12),
+                            to_unsigned(177, 12),
+                            to_unsigned(107, 12),
+                            to_unsigned(75, 12),
+                            to_unsigned(24, 12)
+                            );
+signal DATA: MATRIZ;
+signal START_FLAG: STD_LOGIC;
+signal COUNT: integer range 0 to 20; --Señal interna que representa al contador
+
+begin
+
+process (RST,CLK) --Almacenamiento del dato
+variable RESULT: UNSIGNED (23 downto 0);
+begin
+    if RST ='1' then
+        DATA_O <= (others => '0');
+        START_FLAG <= '0';
+        COUNT <= 0;
+        for I in 0 to 18 loop
+            DATA(I) <= (others => '0');
+        end loop;
+    elsif (CLK'event and CLK = '1') then       
+        if START_I = '1' and COUNT = 0 then           
+            for I in 0 to 17 loop
+                DATA(I + 1) <= DATA(I);
+            end loop;            
+            DATA(0) <= unsigned(DATA_I (11 downto 0));
+            RESULT := (others => '0');
+            COUNT <= COUNT + 1;
+        elsif (START_I = '1' and COUNT > 0) or START_I = '0' then
+            if COUNT = 20 then
+                DATA_O <= "0000" & std_logic_vector(RESULT(23 downto 12));
+                COUNT <= 0;
+            elsif COUNT > 0 and COUNT < 20 then
+                RESULT := RESULT + (COEFF(COUNT-1)*DATA(COUNT-1));
+                COUNT <= COUNT + 1;
+            end if;
+        end if;
+    end if;
+end process;
+
+end Behavioral;
