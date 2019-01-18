9385712e50a1e4e3b153294c64aa3970688fc7a6
Galco6 <victor.galvincoronil@alum.uca.es>
2019-01-19 00:22:59 +0100

    Problema 1

--- Problemas/problema_1.vhd
+++ Problemas/problema_1.vhd
@@ -15,15 +15,30 @@ entity secuence is
     Port ( CLK_I in STD_LOGIC;
            RESET: in STD_LOGIC;
            X_I : in STD_LOGIC;
-           Y_O : out STD_LOGIC
+           Z_O : out STD_LOGIC
          );
 end secuence;
 
 architecture Behavioral of secuence is
 
-signal secuence: STD_LOGIC_VECTOR;
+signal SEC: STD_LOGIC_VECTOR (3 downto 0);
 
 begin
 
+process (RESET_I, CLK_I)
+begin
+    if RESET = '1' then
+        SEC <= "0000"
+    elsif (rising_edge(CLK_I)) then
+        SEC (3 downto 1) <= SEC (2 downto 0)
+        SEC (0) <= X_I;
+        if (SEC = "1101") then
+            Z_O <= 1;
+        else
+            Z_O <= 0;
+        end if;
+    end if;
+    end if;
+end process
 
 end Behavioral;
